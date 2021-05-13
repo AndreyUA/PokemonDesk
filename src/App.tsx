431e140a785fdb51cfe45039fd9eb0639dc8b2e6
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-05-13 11:27:20 +0300

    ts hometask

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import cn from 'classnames';
 import s from './App.module.scss';
 
 const App = () => {
-  console.log('###: Some log');
-
   return <div className={cn(s.header)}>This is App! Yes, we did it!!!</div>;
 };
 
