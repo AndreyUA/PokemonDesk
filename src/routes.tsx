7896d67d431e3d50f38446ccea597aa906bf52aa
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-05-17 14:45:50 +0300

    added routes and active link

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,9 +3,43 @@ import React from 'react';
 import Home from './pages/Home/index.tsx';
 import Empty from './pages/Empty/index.tsx';
 
-const routes = {
-  '/': () => <Home />,
-  '/pokedex': () => <Empty />,
-};
+interface IGeneralMenu {
+  title: string;
+  link: string;
+  component: () => JSX.Element;
+}
+
+export const GENERAL_MENU: Array<IGeneralMenu> = [
+  {
+    title: 'Home',
+    link: '/',
+    component: () => <Home />,
+  },
+  {
+    title: 'Pokédex',
+    link: '/pokedex',
+    component: () => <Empty title="Pokedex" />,
+  },
+  {
+    title: 'Legendaries',
+    link: '/legendaries',
+    component: () => <Empty title="legendaries" />,
+  },
+  {
+    title: 'Documentation',
+    link: '/documentation',
+    component: () => <Empty title="documentation" />,
+  },
+];
+
+interface IAccMenu {
+  [n: string]: () => JSX.Element;
+}
+
+const routes = GENERAL_MENU.reduce((acc: IAccMenu, item: IGeneralMenu) => {
+  acc[item.link] = item.component;
+
+  return acc;
+}, {});
 
 export default routes;
