2e58d0e41b2c776d1ab52be4442ee3a0f09d6b00
Andrey Zhygarev <infernalsteel666@gmail.com>
2021-05-22 10:54:16 +0300

    added custom hook

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -6,22 +6,37 @@ import s from './Pokedex.module.scss';
 import PokemonCard from '../../components/PokemonCard/index.tsx';
 import Heading from '../../components/Heading/index.tsx';
 
-const Pokedex = () => {
-  const [totalPokemons, setTotalPokemons] = useState<number>(0);
-  const [pokemons, setPokemons] = useState<Array<any>>([]);
+const usePokemons = () => {
+  const [data, setData] = useState<any>([]);
   const [isLoading, setIsloading] = useState<boolean>(true);
   const [isError, setIsError] = useState<boolean>(false);
 
   useEffect(() => {
-    fetch('http://zar.hosthot.ru/api/v1/pokemons')
-      .then((response) => response.json())
-      .then((data) => {
-        setTotalPokemons(data.total);
-        setPokemons(data.pokemons);
-      })
-      .catch(() => setIsError(true))
-      .finally(() => setIsloading(false));
+    const getPokemons = async () => {
+      try {
+        const response = await fetch('http://zar.hosthot.ru/api/v1/pokemons');
+        const dataResponse = await response.json();
+
+        setData(dataResponse);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsloading(false);
+      }
+    };
+
+    getPokemons();
   }, []);
+
+  return {
+    isLoading,
+    isError,
+    data,
+  };
+};
+
+const Pokedex = () => {
+  const { isLoading, isError, data } = usePokemons();
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Something wrong...</div>;
@@ -30,10 +45,10 @@ const Pokedex = () => {
     <div className={s.root}>
       <div className={s.wrapper}>
         <Heading type="h1">
-          {totalPokemons} <b>Pokemons</b> for you to choose your favorite
+          {data.totalPokemons} <b>Pokemons</b> for you to choose your favorite
         </Heading>
         <ul className={s.pokemonCards}>
-          {pokemons.map((pokemon: any) => {
+          {data.pokemons.map((pokemon: any) => {
             const { id, name_clean, stats, types, img } = pokemon;
             return (
               <PokemonCard
